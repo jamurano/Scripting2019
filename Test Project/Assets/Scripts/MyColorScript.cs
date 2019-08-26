2dcdd6cae2eec45f5d2cc5e897265ad12b1988e3
Unknown <jamurano@yahoo.com>
2019-08-25 20:53:57 -0600

    Test Project

---
+++ Test Project/Assets/Scripts/MyColorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyColorScript : MonoBehaviour
+{
+
+    public GameObject plane;
+    private MeshRenderer myRenderer;
+
+    private void Start()
+    {
+        Debug.Log("We made it here");
+    }
+
+    public void OnClickChangeColor ()
+    {
+        myRenderer = plane.GetComponent<MeshRenderer>();
+        myRenderer.enabled = !myRenderer.enabled;
+    }
+
+}
