5371b233eb027428368ddf8d47fa8a0c197d6968
Unknown <jamurano@yahoo.com>
2019-09-13 19:44:44 -0600

    Re-doing the stuff we learned in class

---
+++ Practicing/Assets/Scripts/FloatData.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+
+public class FloatData : ScriptableObject
+{
+    public float value = 1f;
+    public float maxValue = 1f;
+
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+    }
+
+    public void UpdateValueLimitZero(float amount)
+    {
+        if(value < 0)
+        {
+            value = 0;
+        }
+        else
+        {
+            UpdateValue(amount);
+        }
+    }
+
+    public void UpdateLimitZeroAndMaxValue(float amount)
+    {
+        if (value < maxValue)
+        {
+            UpdateValue(amount);
+        }
+        else
+        {
+            value = maxValue;
+        }
+        UpdateValueLimitZero(amount);
+    }
+}
---
+++ Practicing/Assets/Scripts/GameActionHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    public UnityEvent handlerEvent;
+
+    private void Start()
+    {
+        gameActionObj.action += Action;
+    }
+
+    private void Action()
+    {
+        handlerEvent.Invoke();
+    }
+}
