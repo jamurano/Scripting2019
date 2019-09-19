05b9621c3dce251967c943c038e0fefe19c9f0d4
Unknown <jamurano@yahoo.com>
2019-09-18 19:59:19 -0600

    Practicing

---
+++ Tower Defense Game/Assets/Destructable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructable : MonoBehaviour
+{
+    public float health = 100;
+    public float damagePerCollision = 40;
+
+    public DestructionSequence destructionSequence;
+
+    public void TakeDamage(float damage)
+    {
+        if (health < 0) return;
+
+        health -= damage;
+
+        if (health < 0 && destructionSequence)
+            StartCoroutine(destructionSequence.SequenceCoroutine(this));
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TakeDamage(damagePerCollision);
+        }
+    }
+}
