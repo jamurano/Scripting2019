c01ed3288e5b170b8bbc3f6765f4c5a770b3daec
Unknown <jamurano@yahoo.com>
2019-09-23 21:50:02 -0600

    Mana Script

---
+++ Tower Defense Game/Assets/ManaBar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+    public float maxMana;
+    public float pointIncreasedPerSecond;
+    public float manaCost;
+    public float updatedMana;
+    public Text manaUi;
+
+    void Start()
+    {
+        pointIncreasedPerSecond = 1;
+        updatedMana = 100;
+        maxMana = 100;
+        manaCost = 25;
+    }
+
+
+    void Update()
+    {
+        updatedMana += pointIncreasedPerSecond * Time.deltaTime;
+
+        if(updatedMana > maxMana)
+        {
+            updatedMana = 100;
+        }
+        if(updatedMana < 0)
+        {
+            updatedMana = 0;
+        }
+        manaUi.text = (int) updatedMana + "Mana";
+    }
+    public void UseMana()
+    {
+        if(updatedMana >= manaCost)
+        {
+            updatedMana = updatedMana - manaCost;
+        }
+    }
+}
