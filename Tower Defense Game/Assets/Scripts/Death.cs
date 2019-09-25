46c52ea52f1c445e682b78c8d10e5bfb2ec386f7
Unknown <jamurano@yahoo.com>
2019-09-25 12:09:17 -0600

    Death, Health, Movement

---
+++ Tower Defense Game/Assets/Scripts/Death.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    public bool isTower;
+    public bool isEnemy;
+    private FloatData Health;
+
+    void Start()
+    {
+        Health = gameObject.GetComponent<Health>();
+    }
+
+    void Update()
+    {
+        if (Health.health <= 0)
+        {
+            if (isTower)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
