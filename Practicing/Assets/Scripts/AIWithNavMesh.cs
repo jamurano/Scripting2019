93fd130c8fe9582e6c8353dbc00ad69b4bcd057e
Unknown <jamurano@yahoo.com>
2019-09-30 21:43:20 -0600

    Menu Buttons Working

---
+++ Practicing/Assets/Scripts/AIWithNavMesh.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AIWithNavMesh : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+
+    public Transform destination;
+
+    void Start()
+    {
+        destination = transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        destination = player;
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        destination = transform;
+    }
+
+    void Update()
+    {
+        agent.destination = destination.position;
+    }
+}
