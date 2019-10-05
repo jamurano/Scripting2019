56263588a292cc2641a2484d2377e8bbedbc2ecc
Unknown <jamurano@yahoo.com>
2019-10-04 20:44:41 -0600

    Worked on Projectile function

--- Tower Defense Game/Assets/Scripts/Projectile.cs
+++ Tower Defense Game/Assets/Scripts/Projectile.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public float coolDown;
+    private float cool;
+    public GameObject projectile;
 
     void Start()
     {
-        
+        cool = coolDown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (coolDown > 0)
+        {
+            coolDown -= Time.deltaTime;
+        }
+        else
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.right, out hit, 15)
+            {
+                if (hit.transform.tag == "enemy")
+                {
+
+                }
+                cool = coolDown;
+                Instantiate(projectile, transform.position, Quaternion.identity);
+            }
+        }
     }
 }
---
+++ Tower Defense Game/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject prefab;
+    void Start()
+    {
+        Vector3 position = new Vector3(45, Random.Range(16.0f, -16.0f), -10);
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
