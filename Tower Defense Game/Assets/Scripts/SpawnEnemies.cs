aba947a8b5dc466dd1037e0a62ac10c0a3a0d181
Unknown <jamurano@yahoo.com>
2019-10-07 19:52:01 -0600

    Working on Enemy Spawn Point

--- Tower Defense Game/Assets/Scripts/SpawnEnemies.cs
+++ Tower Defense Game/Assets/Scripts/SpawnEnemies.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SpawnEnemies : MonoBehaviour
 {
     public GameObject prefab;
+
     void Start()
     {
-        Vector3 position = new Vector3(45, Random.Range(16.0f, -16.0f), -10);
+    Vector3 position = new Vector3(45, Random.Range(16.0f, -16.0f), -10);
         Instantiate(prefab, position, Quaternion.identity);
     }
 }
