66d38e56ce1c238e8dace17a41912fde481965c1
Unknown <jamurano@yahoo.com>
2019-10-10 19:39:28 -0600

    Worked on Spawn Points More

---
+++ Tower Defense Game/Assets/Scripts/SpawnProjectile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectile : MonoBehaviour
+{
+    public void SpawnP()
+    {
+        print("Projectile Spawned");
+    }
+}
