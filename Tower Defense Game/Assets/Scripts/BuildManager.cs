7b72aecfe36da9d8e138dafa7ecf5ed5ccbb6af4
Unknown <jamurano@yahoo.com>
2019-10-24 20:33:18 -0600

    Working on Tower Builder

---
+++ Tower Defense Game/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    
+    private GameObject towerToBuild;
+
+    public GameObject GetTowerToBuild()
+    {
+        return towerToBuild;
+    }
+}
---
+++ Tower Defense Game/Assets/Scripts/TowerPlacer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerPlacer : MonoBehaviour
+{
+    public Color hoverColor;
+    private Color startColor;
+
+    public Button towerSprite;
+
+    public bool canSelect;
+
+    private void Start()
+    {
+        
+    }
+    public void OnMouseDown()
+    {
+        Button icon = towerSprite.GetComponent<Button>();
+        //OnMouseDown will select a tower from the Sprites available
+    }
+
+    public void OnMouseDrag()
+    {
+        //OnMouseDrag allows you to drag the selection to the grid
+    }
+    public void OnMouseEnter()
+    {
+        //hoverColor
+    }
+
+    private void OnMouseExit()
+    {
+        //returns to normal color
+    }
+}
